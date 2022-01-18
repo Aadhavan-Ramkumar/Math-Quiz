8c21f30bfbde0ee6972baa1183528ac050728c2f
Aadhavan-Ramkumar <74062499+Aadhavan-Ramkumar@users.noreply.github.com>
2022-01-18 19:55:41 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -17,18 +17,43 @@ document.getElementById("User2Score").innerHTML = User2Score;
 document.getElementById("PlayerQuestion").innerHTML = "Question - " + User1Name;
 document.getElementById("PlayerAnswer").innerHTML = "Answer - " + User2Name;
 
+QuestionOperator = "";
+AnswerOperator = "";
+
+function Operation(Operation, HumanOperator, ComputerOperator) {
+    console.log(Operation, HumanOperator, ComputerOperator);
+    document.getElementById("CurrentOperation").innerHTML = "Current Operation: " + Operation
+    QuestionOperator = HumanOperator;
+    AnswerOperator = ComputerOperator;
+}
+
 function Send() {
     Number1 = document.getElementById("Number1").value;
     Number2 = document.getElementById("Number2").value;
-    Answer = parseInt(Number1) * parseInt(Number2);
+    switch (AnswerOperator) {
+        case "+":
+            Answer = parseInt(Number1) + parseInt(Number2);
+            break;
+        case "-":
+            Answer = parseInt(Number1) - parseInt(Number2);
+            break;
+        case "*":
+            Answer = parseInt(Number1) * parseInt(Number2);
+            break;
+        case "/":
+            Answer = parseInt(Number1) / parseInt(Number2);
+            break;
+    }
+    console.log(Answer);
 
-    Question = "<h4>" + Number1 + " X " + Number2 + "</h4>";
+    Question = "<h4>" + Number1 + QuestionOperator + Number2 + "</h4>";
     Input = "<br> Answer: <input type='text' id='InputCheck'>";
     Check = "<br><br><button class='btn btn-info' onclick='CheckAnswer()'> Check </button>";
     Row = Question + Input + Check;
     document.getElementById("Output").innerHTML = Row;
     document.getElementById("Number1").value = "";
     document.getElementById("Number2").value = "";
+    document.getElementById("CurrentOperation").innerHTML = "";
 }
 
 PlayerQuestion = "Player1";
